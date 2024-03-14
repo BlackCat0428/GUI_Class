e40566d538631a1419806b9665670a053d574c4b
BlackCat <ries3330@gmail.com>
2024-03-14 21:17:26 +0800

    Form Button

---
+++ N10310027_GUI_Class/N10310027_GUI_Class/FormButton.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace N10310027_GUI_Class
+{
+    partial class FormButton
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(38, 102);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(100, 40);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "請按我一下";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // FormButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 600);
+            this.Controls.Add(this.btnClick);
+            this.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormButton";
+            this.Text = "按鈕";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+    }
+}
--- N10310027_GUI_Class/N10310027_GUI_Class/FormMain.Designer.cs
+++ N10310027_GUI_Class/N10310027_GUI_Class/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace N10310027_GUI_Class
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -31,9 +31,7 @@
             this.label_Class = new System.Windows.Forms.Label();
             this.label_ID = new System.Windows.Forms.Label();
             this.label_Name = new System.Windows.Forms.Label();
-            this.label_Num = new System.Windows.Forms.Label();
-            this.btn_Add = new System.Windows.Forms.Button();
-            this.btn_Sub = new System.Windows.Forms.Button();
+            this.btnButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label_Class
@@ -41,7 +39,8 @@
             this.label_Class.AutoSize = true;
             this.label_Class.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label_Class.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.label_Class.Location = new System.Drawing.Point(12, 9);
+            this.label_Class.Location = new System.Drawing.Point(16, 12);
+            this.label_Class.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Class.Name = "label_Class";
             this.label_Class.Size = new System.Drawing.Size(142, 32);
             this.label_Class.TabIndex = 0;
@@ -52,7 +51,8 @@
             this.label_ID.AutoSize = true;
             this.label_ID.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label_ID.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.label_ID.Location = new System.Drawing.Point(326, 9);
+            this.label_ID.Location = new System.Drawing.Point(435, 12);
+            this.label_ID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_ID.Name = "label_ID";
             this.label_ID.Size = new System.Drawing.Size(158, 32);
             this.label_ID.TabIndex = 1;
@@ -63,57 +63,37 @@
             this.label_Name.AutoSize = true;
             this.label_Name.Font = new System.Drawing.Font("標楷體", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label_Name.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.label_Name.Location = new System.Drawing.Point(678, 9);
+            this.label_Name.Location = new System.Drawing.Point(904, 12);
+            this.label_Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label_Name.Name = "label_Name";
             this.label_Name.Size = new System.Drawing.Size(110, 32);
             this.label_Name.TabIndex = 2;
             this.label_Name.Text = "謝羽紋";
             // 
-            // label_Num
+            // btnButton
             // 
-            this.label_Num.AutoSize = true;
-            this.label_Num.Font = new System.Drawing.Font("標楷體", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label_Num.Location = new System.Drawing.Point(376, 160);
-            this.label_Num.Name = "label_Num";
-            this.label_Num.Size = new System.Drawing.Size(44, 48);
-            this.label_Num.TabIndex = 3;
-            this.label_Num.Text = "0";
+            this.btnButton.Location = new System.Drawing.Point(25, 174);
+            this.btnButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnButton.Name = "btnButton";
+            this.btnButton.Size = new System.Drawing.Size(133, 53);
+            this.btnButton.TabIndex = 3;
+            this.btnButton.Text = "按鈕";
+            this.btnButton.UseVisualStyleBackColor = true;
+            this.btnButton.Click += new System.EventHandler(this.btnButton_Click);
             // 
-            // btn_Add
+            // FormMain
             // 
-            this.btn_Add.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btn_Add.Location = new System.Drawing.Point(270, 314);
-            this.btn_Add.Name = "btn_Add";
-            this.btn_Add.Size = new System.Drawing.Size(80, 40);
-            this.btn_Add.TabIndex = 4;
-            this.btn_Add.Text = "加1";
-            this.btn_Add.UseVisualStyleBackColor = true;
-            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
-            // 
-            // btn_Sub
-            // 
-            this.btn_Sub.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btn_Sub.Location = new System.Drawing.Point(451, 314);
-            this.btn_Sub.Name = "btn_Sub";
-            this.btn_Sub.Size = new System.Drawing.Size(80, 40);
-            this.btn_Sub.TabIndex = 5;
-            this.btn_Sub.Text = "減1";
-            this.btn_Sub.UseVisualStyleBackColor = true;
-            this.btn_Sub.Click += new System.EventHandler(this.btn_Sub_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btn_Sub);
-            this.Controls.Add(this.btn_Add);
-            this.Controls.Add(this.label_Num);
+            this.ClientSize = new System.Drawing.Size(1067, 600);
+            this.Controls.Add(this.btnButton);
             this.Controls.Add(this.label_Name);
             this.Controls.Add(this.label_ID);
             this.Controls.Add(this.label_Class);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormMain";
+            this.Text = "GUI主程式";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,9 +104,7 @@
         private System.Windows.Forms.Label label_Class;
         private System.Windows.Forms.Label label_ID;
         private System.Windows.Forms.Label label_Name;
-        private System.Windows.Forms.Label label_Num;
-        private System.Windows.Forms.Button btn_Add;
-        private System.Windows.Forms.Button btn_Sub;
+        private System.Windows.Forms.Button btnButton;
     }
 }
 
