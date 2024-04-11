774177925bee9144744e990dfae4f4f387c059f3
BlackCat <ries3330@gmail.com>
2024-04-11 21:14:19 +0800

    TextBox Count

--- N10310027_GUI_Class/N10310027_GUI_Class/FormTextBox.Designer.cs
+++ N10310027_GUI_Class/N10310027_GUI_Class/FormTextBox.Designer.cs
@@ -31,20 +31,45 @@
             this.tbInput = new System.Windows.Forms.TextBox();
             this.btnInput = new System.Windows.Forms.Button();
             this.labInput = new System.Windows.Forms.Label();
+            this.tbCount1 = new System.Windows.Forms.TextBox();
+            this.tbCount2 = new System.Windows.Forms.TextBox();
+            this.labAdd = new System.Windows.Forms.Label();
+            this.labEqual = new System.Windows.Forms.Label();
+            this.labAns = new System.Windows.Forms.Label();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.btnCount2 = new System.Windows.Forms.Button();
+            this.labAns2 = new System.Windows.Forms.Label();
+            this.labEqual2 = new System.Windows.Forms.Label();
+            this.labMin = new System.Windows.Forms.Label();
+            this.tbCount4 = new System.Windows.Forms.TextBox();
+            this.tbCount3 = new System.Windows.Forms.TextBox();
+            this.btnCount3 = new System.Windows.Forms.Button();
+            this.labAns3 = new System.Windows.Forms.Label();
+            this.labEqual3 = new System.Windows.Forms.Label();
+            this.labx = new System.Windows.Forms.Label();
+            this.tbCount6 = new System.Windows.Forms.TextBox();
+            this.tbCount5 = new System.Windows.Forms.TextBox();
+            this.btnCount4 = new System.Windows.Forms.Button();
+            this.labAns4 = new System.Windows.Forms.Label();
+            this.labEqual4 = new System.Windows.Forms.Label();
+            this.labDiv = new System.Windows.Forms.Label();
+            this.tbCount8 = new System.Windows.Forms.TextBox();
+            this.tbCount7 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tbInput
             // 
-            this.tbInput.Location = new System.Drawing.Point(13, 109);
+            this.tbInput.Location = new System.Drawing.Point(12, 41);
             this.tbInput.Name = "tbInput";
             this.tbInput.Size = new System.Drawing.Size(120, 22);
             this.tbInput.TabIndex = 0;
             // 
             // btnInput
             // 
-            this.btnInput.Location = new System.Drawing.Point(13, 170);
+            this.btnInput.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnInput.Location = new System.Drawing.Point(12, 79);
             this.btnInput.Name = "btnInput";
-            this.btnInput.Size = new System.Drawing.Size(120, 20);
+            this.btnInput.Size = new System.Drawing.Size(120, 27);
             this.btnInput.TabIndex = 1;
             this.btnInput.Text = "確定";
             this.btnInput.UseVisualStyleBackColor = true;
@@ -53,17 +78,261 @@
             // labInput
             // 
             this.labInput.AutoSize = true;
-            this.labInput.Location = new System.Drawing.Point(13, 59);
+            this.labInput.Location = new System.Drawing.Point(13, 26);
             this.labInput.Name = "labInput";
             this.labInput.Size = new System.Drawing.Size(41, 12);
             this.labInput.TabIndex = 2;
             this.labInput.Text = "請輸入";
+            // 
+            // tbCount1
+            // 
+            this.tbCount1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount1.Location = new System.Drawing.Point(181, 52);
+            this.tbCount1.Name = "tbCount1";
+            this.tbCount1.Size = new System.Drawing.Size(100, 21);
+            this.tbCount1.TabIndex = 3;
+            // 
+            // tbCount2
+            // 
+            this.tbCount2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount2.Location = new System.Drawing.Point(304, 52);
+            this.tbCount2.Name = "tbCount2";
+            this.tbCount2.Size = new System.Drawing.Size(100, 21);
+            this.tbCount2.TabIndex = 4;
+            // 
+            // labAdd
+            // 
+            this.labAdd.AutoSize = true;
+            this.labAdd.Location = new System.Drawing.Point(287, 55);
+            this.labAdd.Name = "labAdd";
+            this.labAdd.Size = new System.Drawing.Size(11, 12);
+            this.labAdd.TabIndex = 5;
+            this.labAdd.Text = "+";
+            // 
+            // labEqual
+            // 
+            this.labEqual.AutoSize = true;
+            this.labEqual.Location = new System.Drawing.Point(410, 55);
+            this.labEqual.Name = "labEqual";
+            this.labEqual.Size = new System.Drawing.Size(11, 12);
+            this.labEqual.TabIndex = 6;
+            this.labEqual.Text = "=";
+            // 
+            // labAns
+            // 
+            this.labAns.AutoSize = true;
+            this.labAns.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labAns.Location = new System.Drawing.Point(427, 55);
+            this.labAns.Name = "labAns";
+            this.labAns.Size = new System.Drawing.Size(12, 16);
+            this.labAns.TabIndex = 7;
+            this.labAns.Text = "?";
+            // 
+            // btnCount
+            // 
+            this.btnCount.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCount.Location = new System.Drawing.Point(455, 52);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(75, 23);
+            this.btnCount.TabIndex = 8;
+            this.btnCount.Text = "計算";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // btnCount2
+            // 
+            this.btnCount2.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCount2.Location = new System.Drawing.Point(455, 108);
+            this.btnCount2.Name = "btnCount2";
+            this.btnCount2.Size = new System.Drawing.Size(75, 23);
+            this.btnCount2.TabIndex = 14;
+            this.btnCount2.Text = "計算";
+            this.btnCount2.UseVisualStyleBackColor = true;
+            this.btnCount2.Click += new System.EventHandler(this.btnCount2_Click);
+            // 
+            // labAns2
+            // 
+            this.labAns2.AutoSize = true;
+            this.labAns2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labAns2.Location = new System.Drawing.Point(427, 111);
+            this.labAns2.Name = "labAns2";
+            this.labAns2.Size = new System.Drawing.Size(12, 16);
+            this.labAns2.TabIndex = 13;
+            this.labAns2.Text = "?";
+            // 
+            // labEqual2
+            // 
+            this.labEqual2.AutoSize = true;
+            this.labEqual2.Location = new System.Drawing.Point(410, 111);
+            this.labEqual2.Name = "labEqual2";
+            this.labEqual2.Size = new System.Drawing.Size(11, 12);
+            this.labEqual2.TabIndex = 12;
+            this.labEqual2.Text = "=";
+            // 
+            // labMin
+            // 
+            this.labMin.AutoSize = true;
+            this.labMin.Location = new System.Drawing.Point(287, 111);
+            this.labMin.Name = "labMin";
+            this.labMin.Size = new System.Drawing.Size(9, 12);
+            this.labMin.TabIndex = 11;
+            this.labMin.Text = "-";
+            // 
+            // tbCount4
+            // 
+            this.tbCount4.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount4.Location = new System.Drawing.Point(304, 108);
+            this.tbCount4.Name = "tbCount4";
+            this.tbCount4.Size = new System.Drawing.Size(100, 21);
+            this.tbCount4.TabIndex = 10;
+            // 
+            // tbCount3
+            // 
+            this.tbCount3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount3.Location = new System.Drawing.Point(181, 108);
+            this.tbCount3.Name = "tbCount3";
+            this.tbCount3.Size = new System.Drawing.Size(100, 21);
+            this.tbCount3.TabIndex = 9;
+            // 
+            // btnCount3
+            // 
+            this.btnCount3.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCount3.Location = new System.Drawing.Point(455, 159);
+            this.btnCount3.Name = "btnCount3";
+            this.btnCount3.Size = new System.Drawing.Size(75, 23);
+            this.btnCount3.TabIndex = 20;
+            this.btnCount3.Text = "計算";
+            this.btnCount3.UseVisualStyleBackColor = true;
+            this.btnCount3.Click += new System.EventHandler(this.btnCount3_Click);
+            // 
+            // labAns3
+            // 
+            this.labAns3.AutoSize = true;
+            this.labAns3.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labAns3.Location = new System.Drawing.Point(427, 162);
+            this.labAns3.Name = "labAns3";
+            this.labAns3.Size = new System.Drawing.Size(12, 16);
+            this.labAns3.TabIndex = 19;
+            this.labAns3.Text = "?";
+            // 
+            // labEqual3
+            // 
+            this.labEqual3.AutoSize = true;
+            this.labEqual3.Location = new System.Drawing.Point(410, 162);
+            this.labEqual3.Name = "labEqual3";
+            this.labEqual3.Size = new System.Drawing.Size(11, 12);
+            this.labEqual3.TabIndex = 18;
+            this.labEqual3.Text = "=";
+            // 
+            // labx
+            // 
+            this.labx.AutoSize = true;
+            this.labx.Location = new System.Drawing.Point(287, 162);
+            this.labx.Name = "labx";
+            this.labx.Size = new System.Drawing.Size(11, 12);
+            this.labx.TabIndex = 17;
+            this.labx.Text = "*";
+            // 
+            // tbCount6
+            // 
+            this.tbCount6.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount6.Location = new System.Drawing.Point(304, 159);
+            this.tbCount6.Name = "tbCount6";
+            this.tbCount6.Size = new System.Drawing.Size(100, 21);
+            this.tbCount6.TabIndex = 16;
+            // 
+            // tbCount5
+            // 
+            this.tbCount5.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount5.Location = new System.Drawing.Point(181, 159);
+            this.tbCount5.Name = "tbCount5";
+            this.tbCount5.Size = new System.Drawing.Size(100, 21);
+            this.tbCount5.TabIndex = 15;
+            // 
+            // btnCount4
+            // 
+            this.btnCount4.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnCount4.Location = new System.Drawing.Point(455, 210);
+            this.btnCount4.Name = "btnCount4";
+            this.btnCount4.Size = new System.Drawing.Size(75, 23);
+            this.btnCount4.TabIndex = 26;
+            this.btnCount4.Text = "計算";
+            this.btnCount4.UseVisualStyleBackColor = true;
+            this.btnCount4.Click += new System.EventHandler(this.btnCount4_Click);
+            // 
+            // labAns4
+            // 
+            this.labAns4.AutoSize = true;
+            this.labAns4.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labAns4.Location = new System.Drawing.Point(427, 213);
+            this.labAns4.Name = "labAns4";
+            this.labAns4.Size = new System.Drawing.Size(12, 16);
+            this.labAns4.TabIndex = 25;
+            this.labAns4.Text = "?";
+            // 
+            // labEqual4
+            // 
+            this.labEqual4.AutoSize = true;
+            this.labEqual4.Location = new System.Drawing.Point(410, 213);
+            this.labEqual4.Name = "labEqual4";
+            this.labEqual4.Size = new System.Drawing.Size(11, 12);
+            this.labEqual4.TabIndex = 24;
+            this.labEqual4.Text = "=";
+            // 
+            // labDiv
+            // 
+            this.labDiv.AutoSize = true;
+            this.labDiv.Location = new System.Drawing.Point(287, 213);
+            this.labDiv.Name = "labDiv";
+            this.labDiv.Size = new System.Drawing.Size(8, 12);
+            this.labDiv.TabIndex = 23;
+            this.labDiv.Text = "/";
+            // 
+            // tbCount8
+            // 
+            this.tbCount8.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount8.Location = new System.Drawing.Point(304, 210);
+            this.tbCount8.Name = "tbCount8";
+            this.tbCount8.Size = new System.Drawing.Size(100, 21);
+            this.tbCount8.TabIndex = 22;
+            // 
+            // tbCount7
+            // 
+            this.tbCount7.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCount7.Location = new System.Drawing.Point(181, 210);
+            this.tbCount7.Name = "tbCount7";
+            this.tbCount7.Size = new System.Drawing.Size(100, 21);
+            this.tbCount7.TabIndex = 21;
             // 
             // FormTextBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCount4);
+            this.Controls.Add(this.labAns4);
+            this.Controls.Add(this.labEqual4);
+            this.Controls.Add(this.labDiv);
+            this.Controls.Add(this.tbCount8);
+            this.Controls.Add(this.tbCount7);
+            this.Controls.Add(this.btnCount3);
+            this.Controls.Add(this.labAns3);
+            this.Controls.Add(this.labEqual3);
+            this.Controls.Add(this.labx);
+            this.Controls.Add(this.tbCount6);
+            this.Controls.Add(this.tbCount5);
+            this.Controls.Add(this.btnCount2);
+            this.Controls.Add(this.labAns2);
+            this.Controls.Add(this.labEqual2);
+            this.Controls.Add(this.labMin);
+            this.Controls.Add(this.tbCount4);
+            this.Controls.Add(this.tbCount3);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.labAns);
+            this.Controls.Add(this.labEqual);
+            this.Controls.Add(this.labAdd);
+            this.Controls.Add(this.tbCount2);
+            this.Controls.Add(this.tbCount1);
             this.Controls.Add(this.labInput);
             this.Controls.Add(this.btnInput);
             this.Controls.Add(this.tbInput);
@@ -79,5 +348,29 @@
         private System.Windows.Forms.TextBox tbInput;
         private System.Windows.Forms.Button btnInput;
         private System.Windows.Forms.Label labInput;
+        private System.Windows.Forms.TextBox tbCount1;
+        private System.Windows.Forms.TextBox tbCount2;
+        private System.Windows.Forms.Label labAdd;
+        private System.Windows.Forms.Label labEqual;
+        private System.Windows.Forms.Label labAns;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.Button btnCount2;
+        private System.Windows.Forms.Label labAns2;
+        private System.Windows.Forms.Label labEqual2;
+        private System.Windows.Forms.Label labMin;
+        private System.Windows.Forms.TextBox tbCount4;
+        private System.Windows.Forms.TextBox tbCount3;
+        private System.Windows.Forms.Button btnCount3;
+        private System.Windows.Forms.Label labAns3;
+        private System.Windows.Forms.Label labEqual3;
+        private System.Windows.Forms.Label labx;
+        private System.Windows.Forms.TextBox tbCount6;
+        private System.Windows.Forms.TextBox tbCount5;
+        private System.Windows.Forms.Button btnCount4;
+        private System.Windows.Forms.Label labAns4;
+        private System.Windows.Forms.Label labEqual4;
+        private System.Windows.Forms.Label labDiv;
+        private System.Windows.Forms.TextBox tbCount8;
+        private System.Windows.Forms.TextBox tbCount7;
     }
 }
--- N10310027_GUI_Class/N10310027_GUI_Class/FormTextBox.cs
+++ N10310027_GUI_Class/N10310027_GUI_Class/FormTextBox.cs
@@ -21,5 +21,38 @@ namespace N10310027_GUI_Class
         {
             labInput.Text = tbInput.Text;
         }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(tbCount1.Text);
+            int num2 = Int32.Parse(tbCount2.Text);
+
+            labAns.Text = (num1 + num2).ToString();
+        }
+
+        private void btnCount2_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(tbCount3.Text);
+            int num2 = Int32.Parse(tbCount4.Text);
+
+            labAns2.Text = (num1 - num2).ToString();
+
+        }
+
+        private void btnCount3_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(tbCount5.Text);
+            int num2 = Int32.Parse(tbCount6.Text);
+
+            labAns3.Text = (num1 * num2).ToString();
+        }
+
+        private void btnCount4_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(tbCount7.Text);
+            int num2 = Int32.Parse(tbCount8.Text);
+
+            labAns4.Text = (num1 / num2).ToString();
+        }
     }
 }
